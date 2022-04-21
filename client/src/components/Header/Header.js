093e5d50d454bb0a85a4f687302584810d44f143
Nilankoor Biswas <nilankoorb@gmail.com>
2022-04-22 01:17:30 +0530

    NavBar Change #3

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import react from "react";
+
 import classes from "./Header.module.css";
 
 export default function Header() {
@@ -7,11 +7,11 @@ export default function Header() {
       <div className={classes.headerContent}>
         <div className={classes.brandName}>MediLine</div>
         <div className={classes.navList}>
-          <div className={classes.navPills}>Home</div>
-          <div className={classes.navPills}>Orders</div>
-          <div className={classes.navPills}>Cart</div>
-          <div className={classes.navPills}>About</div>
-          <button className={classes.authButton}>Login / SignUp</button>
+          <div className={classes.navPills}>HOME</div>
+          <div className={classes.navPills}>ORDERS</div>
+          <div className={classes.navPills}>CART</div>
+          <div className={classes.navPills}>ABOUT</div>
+          <button className={classes.authButton}>LOGIN | SIGN UP</button>
         </div>
       </div>
     </header>
