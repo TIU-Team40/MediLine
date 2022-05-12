aecf7323c637009639c56d6f4a2104ad34c14539
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-05-12 19:27:55 +0530

    removed extra spaces from div element

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,11 +7,11 @@ export default function Header() {
       <div className={classes.headerContent}>
         <div className={classes.brandName}>MediLine</div>
         <div className={classes.navList}>
-          <div className={classes.navPills}>Home</div>
-          <div className={classes.navPills}>Orders</div>
-          <div className={classes.navPills}>Cart</div>
-          <div className={classes.navPills}>About</div>
-          <button className={classes.authButton}>Login / SignUp</button>
+        <div className={classes.navPills}>Home</div>
+        <div className={classes.navPills}>Orders</div>
+        <div className={classes.navPills}>Cart</div>
+        <div className={classes.navPills}>About</div>
+        <button className={classes.authButton}>Login / SignUp</button>
         </div>
       </div>
     </header>
