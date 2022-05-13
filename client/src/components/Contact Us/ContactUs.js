465395a171c4c95daeac817f717ae5736007e09a
RISHAB JAIN <42496717+Rishab1444@users.noreply.github.com>
2022-05-13 18:41:44 +0530

    Some minor changes updated

--- client/src/components/Contact Us/ContactUs.js
+++ client/src/components/Contact Us/ContactUs.js
@@ -7,20 +7,21 @@ export default function Contact ()
         <div>
         <body>
             <h1 className="Contactus">Contact Us</h1>
-            <p className="content"> Hello User, We are here to answer your every query .If you want to reach us either  
-                 you can email us on </p><p className="content"> Customer care email-id given below or you can call us at our 
-                Customer Care which is  24 x 7 available for you. </p>
+            <p className="content"> Hello User, We are here to answer your every queries .To reach us either  
+                 you can email us on our </p><p className="content"> email-id  or even call us at our 
+                Customer Care number. We are 24 x 7 available for you. </p>
                 <p className="email">
                 Email id :  medilineforyou@MediLine.com;
                 </p>
-                <p className="number">
-                Customer Care number : 18110010101
+                <p className="email">
+                Phone-number : 18110010101
                   </p>
 
                 <h1 className="Contactus"> Our Location </h1>
                 <img src={location} alt ="location" />
                 
-                </body>
+                <p className="email" >Headquatered :Sector-V, Kolkata</p> 
+                    </body>
 
         </div>
         
