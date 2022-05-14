faa252906ef3560ba037e94a2ad7baefc60b8dfa
Ayush Sharma <ayush20892@gmail.com>
2022-05-14 21:48:12 +0530

    refactor: code updated Ayush

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Contact from "./components/Contact&Location/Contact&Location";
+import Contact from "./components/Contact Us/ContactUs";
 import Header from "./components/Header/Header";
 
 function App() {
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <Header />
       <Contact />
-      {/* Welcome To MediLine */}
     </div>
   );
 }
