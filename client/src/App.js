d981cc9289ddbe67db5072be64d48ca9d3c48266
RISHAB JAIN <42496717+Rishab1444@users.noreply.github.com>
2022-06-05 22:17:44 +0530

    consult added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 import "./App.css";
-<<<<<<< HEAD
-import Contact from "./components/Contact Us/ContactUs";
-import Founder from "./components/founder/Founder.js";
-=======
->>>>>>> aritra
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -14,13 +9,9 @@ import './styles/global.css'
 import ProductListing from "./pages/ProductListing/ProductListing";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
+import Consultadocter from "./pages/ConsultADocteer/ConsultAdocter";
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      
-      <Founder/>
-=======
     <div className="container">
       <Navbar />
        <Routes>
@@ -28,9 +19,9 @@ function App() {
          <Route path="/" element={<Home/>}/>
          <Route path="/products" element={<ProductListing/>}/>
           <Route path="/cart" element={<Cart/>}/>
+          <Route path="/consult"element={<Consultadocter />}/>
        </Routes>
       <Footer/> 
->>>>>>> aritra
     </div>
   );
 }
---
+++ client/src/pages/ConsultADocteer/ConsultAdocter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '../../styles/global.css'
+export default function Consultadocter(){
+
+        <div >
+            <Link to="/">
+            <h2> Consult a Docter</h2>
+
+            <p> Hello UserName </p>
+            <p>
+                Consult with docter online and get diagnosed
+            </p>
+            <textarea placeholder="Write about your symptoms" >
+
+            </textarea>
+            <form>
+            <label >
+            <input className="button"type="submit" />            
+            </label>
+            </form>
+            <img 
+            </Link>
+
+        </div>
+
+
+}
