aa3ae8474e14d1f74d0be5ecdb9d96e7ceee0ba6
RISHAB JAIN <42496717+Rishab1444@users.noreply.github.com>
2022-06-06 17:22:54 +0530

    Conuslt page added

--- client/src/pages/ConsultADocteer/ConsultAdocter.js
+++ client/src/pages/ConsultADocteer/ConsultAdocter.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../styles/global.css'
+import '../../styles/global.css';
+import classes from './ConsultAdocter.css';
+import img from './docterss.jpeg';
+
 export default function Consultadocter(){
+    return(
+    <div>
+      
+    <h2 className={classes.heading}>Consult a Docter!</h2>
+    <p className = {classes.hello}> Hello Username ! <br>
+    </br>Consult with docter online and get diagnosed </p> 
+    <textarea className = {classes.aa}  placeholder = "Write about your symptoms" /> 
+    <img src  ={img} className = {classes.img} alt ="docterss" /> 
+    <form>
+      
+      <input className ={classes.button} type = "submit" value = "Consult" />
+      </form>    
+  </div>
+);
 
-        <div >
-            <Link to="/">
-            <h2> Consult a Docter</h2>
-
-            <p> Hello UserName </p>
-            <p>
-                Consult with docter online and get diagnosed
-            </p>
-            <textarea placeholder="Write about your symptoms" >
-
-            </textarea>
-            <form>
-            <label >
-            <input className="button"type="submit" />            
-            </label>
-            </form>
-            <img 
-            </Link>
-
-        </div>
+        
 
 
 }
