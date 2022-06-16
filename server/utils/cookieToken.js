ed1a60e123304905b0cfecd21f78478a595ecdf1
Ayush Sharma <ayush20892@gmail.com>
2022-06-16 22:41:10 +0530

    cookie property updated

--- server/utils/cookieToken.js
+++ server/utils/cookieToken.js
@@ -4,8 +4,8 @@ module.exports = async (user, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRY * 60 * 60 * 1000),
     httpOnly: true,
-    // sameSite: "none",
-    // secure: true,
+    sameSite: "none",
+    secure: true,
   };
 
   user.password = undefined;
